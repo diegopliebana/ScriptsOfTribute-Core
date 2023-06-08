d66cd028caac535e84ebd353dd63e34e8003c010
diegopliebana <akilascartas@gmail.com>
2023-06-08 16:31:46 +0100

    Skeleton for ElasticMCTS
    New files:
     - IHerustic: interface to facilitate trying different heuristics.
     - ElasticMCTSDefaultHeuristic: original MCTSBot state evaluation heuristic.
     - ElasticMCTSUtils: util functions that are not exclusive to MCTS.

---
+++ Bots/ElasticMCTSUtils.cs
@@ -0,0 +1,44 @@
+﻿using ScriptsOfTribute;
+using ScriptsOfTribute.Board.CardAction;
+using ScriptsOfTribute.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bots
+{
+    class ElasticMCTSUtils
+    {
+        // Returns all possible moves with the exception of the END_TURN
+        public static List<Move> NotEndTurnPossibleMoves(List<Move> possibleMoves)
+        {
+            return possibleMoves.Where(m => m.Command != CommandEnum.END_TURN).ToList();
+        }
+
+        // Retuns one of the possible moves from the list received as parameter at random. 
+        // Prioritizes not END_TURN moves.
+        public static Move DrawNextMove(List<Move> possibleMoves, SeededGameState gameState, SeededRandom rng)
+        {
+            Move nextMove;
+            List<Move> notEndTurnPossibleMoves = ElasticMCTSUtils.NotEndTurnPossibleMoves(possibleMoves);
+            if (notEndTurnPossibleMoves.Count > 0)
+            {
+                if ((gameState.BoardState == BoardState.NORMAL) && (Extensions.RandomK(0, 10000, rng) == 0))
+                {
+                    nextMove = Move.EndTurn();
+                }
+                else
+                {
+                    nextMove = notEndTurnPossibleMoves.PickRandom(rng);
+                }
+            }
+            else
+            {
+                nextMove = Move.EndTurn();
+            }
+            return nextMove;
+        }
+
+
+    }
+}
---
+++ Bots/IHeuristic.cs
@@ -0,0 +1,13 @@
+﻿using ScriptsOfTribute.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bots
+{
+    public interface IHeuristic
+    {
+        public double Value(SeededGameState gameState);
+
+    }
+}
